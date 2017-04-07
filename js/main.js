83b487f2efec253cd98ef0951ba8372dd5dcca14
Dan Scott <dj-scott1@hotmail.co.uk>
2017-04-07 20:51:04 +0100

    Added the nudge up hover effect for navbar

--- js/main.js
+++ js/main.js
@@ -1,6 +1,17 @@
 $(document).ready(function(){
     $('.parallax').parallax();
-    $('.btn').on('click', function(){
-      alert("image clicked");
-    })
-  });
+
+    // Need to declare img twice because the first img is only local to first function
+    $('li').hover(function() { //mouse in
+      var img = $(this).find('img');
+	    $(img).animate({ top: '-20px' }, 400);
+	  }, function() { //mouse out
+      var img = $(this).find('img');
+	    $(img).animate({ top: 0 }, 400);
+	  });
+
+	$('#nav-icon1').click(function(){
+		$(this).toggleClass('open');
+	});
+
+});
