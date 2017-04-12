21a3d28abf6bbadeb8a46dbd5c61ab6baf29ec47
Dan Scott <dj-scott1@hotmail.co.uk>
2017-04-12 17:03:54 +0100

    Added function to put the active class based on which page the user is on and moved the slideout js stuff to this file

--- js/main.js
+++ js/main.js
@@ -1,4 +1,16 @@
 $(document).ready(function(){
+
+    var slideout = new Slideout({
+       'panel': document.getElementById('panel'),
+       'menu': document.getElementById('menu'),
+       'padding': 256,
+       'tolerance': 70
+     });
+     $('.toggle-button').on('click', function() {
+      slideout.toggle();
+    });
+
+    slideout.enableTouch();
     $('.parallax').parallax();
 
     // Need to declare img twice because the first img is only local to first function
@@ -13,5 +25,22 @@ $(document).ready(function(){
 	$('#nav-icon1').click(function(){
 		$(this).toggleClass('open');
 	});
+
+  if($('html').hasClass('slideout-open')){
+    alert('sidenav opened');
+    $('body').addClass('dim-page')
+  }
+
+  // Add the active class to the navbar item's page
+  // I.e if on contact, put active on the contact me navbar list item
+  var url = window.location.pathname,
+       urlRegExp = new RegExp(url.replace(/\/$/,'') + "$"); // create regexp to match current url pathname and remove trailing slash if present as it could collide with the link in navigation in case trailing slash wasn't present there
+       // now grab every link from the navigation
+       $('.nav-list a').each(function(){
+           // and test its normalized href against the url pathname regexp
+           if(urlRegExp.test(this.href.replace(/\/$/,''))){
+               $(this).addClass('active');
+           }
+       });
 
 });
