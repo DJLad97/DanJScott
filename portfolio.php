ccf6f7b676a63292256def8a0f0d14afabf27b95
Dan Scott <dj-scott1@hotmail.co.uk>
2017-04-12 21:06:54 +0100

    Added barebones portfolio page

---
+++ portfolio.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <?php include('includes/head.php'); ?>
+  <title>Portfolio :: Dan Scott</title>
+</head>
+<body>
+  <?php include('includes/sidenav.php'); ?>
+  <main id="panel">
+    <?php include('includes/navbar.php'); ?>
+
+  </main>
+</body>
+</html>
