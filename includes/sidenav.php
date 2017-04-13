a2f9e6514a4881db4bc230178838b2ad800053c2
Dan Scott <dj-scott1@hotmail.co.uk>
2017-04-13 20:00:11 +0100

    Fixed href links

--- includes/sidenav.php
+++ includes/sidenav.php
@@ -1,25 +1,25 @@
     <div id="menu" class="grey lighten-3">
-      <ul>
+      <ul id="sidenav">
         <li class="sidenav-item">
-          <a href="index.html">
+          <a href="index.php">
             <img src="images/about-me-blue.png" class="about-me-sidenav" width="48" height="48" alt="image not found">
             <span class="sidenav-caption">About Me</span>
           </a>
         </li>
         <li class="sidenav-item">
-          <a href="portfolio.html">
+          <a href="portfolio.php">
             <img src="images/portfolio-icon-blue.png" class="portfolio-sidenav" width="48" height="48" alt="image not found">
             <span class="sidenav-caption">Portfolio</span>
           </a>
         </li>
         <li class="sidenav-item">
-          <a href="blog.html">
+          <a href="blog.php">
             <img src="images/blog-icon-blue.png" class="blog-sidenav" width="48" height="48" alt="image not found">
             <span class="sidenav-caption">Blog</span>
           </a>
         </li>
         <li class="sidenav-item">
-          <a href="contact-me.html">
+          <a href="contact-me.php">
             <img src="images/contact-me-icon-blue.png" class="contact-me-sidenav" width="48" height="48" alt="image not found">
             <span class="sidenav-caption">Contact</span>
           </a>
