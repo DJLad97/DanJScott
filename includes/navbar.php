60de038d73c9151b97c14eb02f578a7d7a581a34
Dan Scott <dj-scott1@hotmail.co.uk>
2017-04-18 21:00:47 +0100

    Stuff

--- includes/navbar.php
+++ includes/navbar.php
@@ -1,20 +1,20 @@
     <div class="container nav-list">
       <ul>
         <li id="about-me">
-          <a href="/" class="box active">
+          <a href="index.php">
             <img src="images/about-me-icon.png" class="about-me-img" width="128" height="128" alt="image not found">
             <span class="caption">About Me</span>
           </a>
         </li>
         <li id="portfolio">
           <a href="portfolio.php">
-            <img src="images/portfolio-icon-white.png" class="portfolio" width="128" height="128" alt="image not found">
+            <img src="images/portfolio-icon-white.png" class="portfolio" width="120" height="120" alt="image not found">
             <span class="caption">Portfolio</span>
           </a>
         </li>
         <li id="blog">
           <a href="blog.php">
-            <img src="images/blog-icon.png" class="blog" width="128" height="128" alt="image not found">
+            <img src="images/blog-icon.png" class="blog" width="120" height="120" alt="image not found">
             <span class="caption">Blog</span>
           </a>
         </li>
