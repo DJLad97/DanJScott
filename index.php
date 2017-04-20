f3b854cb97e890bf154696febcdff72458fa7530
Dan Scott <dj-scott1@hotmail.co.uk>
2017-04-20 20:36:02 +0100

    Added grid with (soon to be text) describing myself (with icons) Also centered my profile image

--- index.php
+++ index.php
@@ -9,6 +9,7 @@ if(empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "off"){
 <!DOCTYPE html>
 <html lang="en">
   <head>
+    <script src="https://use.fontawesome.com/1a6d4ae9a2.js"></script>
     <?php include('includes/head.php'); ?>
     <title>About :: Dan Scott</title>
   </head>
@@ -52,15 +53,108 @@ if(empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] == "off"){
 
       <div class="section grey lighten-3">
         <div class="row container">
-          <div class="border">
-            <h2 class="section-header">Who Am I?</h2>
-            <div id="profile-img">
-              <div class="circular-landscape">
-                <img src="images/me.jpg" alt="profile image">
+          <!-- <h2 class="section-header">Who Am I?</h2> -->
+          <div id="profile-img">
+            <div class="circular-landscape">
+              <img src="images/me.jpg" alt="profile image">
+            </div>
+          </div>
+          <div class="desc">
+            <p class="border">
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ipsum felis, efficitur ac auctor ut,
+              ultricies egestas lectus. Fusce eget fringilla urna. Cras pharetra, nisl et venenatis facilisis,
+              purus eros ullamcorper nunc, ut aliquam orci ipsum in purus. Integer molestie diam nec tellus viverra fermentum.
+              Nunc bibendum felis ante, condimentum lacinia nulla egestas quis. In feugiat dolor a massa blandit molestie.
+              Vivamus sollicitudin massa ut lobortis vestibulum. Etiam a purus dignissim, vehicula libero vel, blandit lorem.
+              Suspendisse luctus blandit ligula vel eleifend. Ut ac urna libero. Aenean semper mollis augue, ut tempus nulla malesuada tempus.
+            </p>
+          </div>
+
+          <!-- <br>
+          <br> -->
+          <div class="icon-container">
+            <div class="row">
+              <div class="col s12 m4">
+                <div class="icon-block">
+                  <div class="center">
+                    <a href="https://github.com/DJLad97" target="_blank">
+                      <i class="fa fa-github icon" aria-hidden="true"></i>
+                    </a>
+                  </div>
+                  <h5 class="center">Aspiring Web Dev</h5>
+                  <p class="icon-block-text">
+                      Right now I'm aiming to improve my skills & understanding of web development,
+                      my skills currently consist of basics: HTML, CSS, Bootstrap, JavaScript/jQuery & PHP.
+                      I also have experience in working with databases - MySQL and Oralce.
+                  </p>
+                </div>
+              </div>
+              <div class="col s12 m4">
+                <div class="icon-block">
+                  <div class="center">
+                    <i class="fa fa-university icon" aria-hidden="true"></i>
+                  </div>
+                  <h5 class="center">Software Engineering Student</h5>
+                  <p class="icon-block-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ipsum felis, efficitur ac auctor ut,
+                    ultricies egestas lectus. Fusce eget fringilla urna. Cras pharetra, nisl et venenatis facilisis,
+                  </p>
+                </div>
+              </div>
+              <div class="col s12 m4">
+                <div class="icon-block">
+                  <div class="center">
+                    <i class="fa fa-building icon" aria-hidden="true"></i>
+                  </div>
+                  <h5 class="center">Looking for placement</h5>
+                  <p class="icon-block-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ipsum felis, efficitur ac auctor ut,
+                    ultricies egestas lectus. Fusce eget fringilla urna. Cras pharetra, nisl et venenatis facilisis,
+                  </p>
+                </div>
               </div>
             </div>
-            <p><a href="UK-Cycling-Events/index.php">Click here</a> to check out my latest web project</p>
+            <div class="row">
+              <div class="col s12 m4">
+                <div class="icon-block">
+                  <div class="center">
+                    <i class="fa fa-music icon" aria-hidden="true"></i>
+                  </div>
+                  <h5 class="center">Music</h5>
+                  <p class="icon-block-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ipsum felis, efficitur ac auctor ut,
+                    ultricies egestas lectus. Fusce eget fringilla urna. Cras pharetra, nisl et venenatis facilisis,
+                  </p>
+                </div>
+              </div>
+              <div class="col s12 m4">
+                <div class="icon-block">
+                  <div class="center">
+                    <i class="fa fa-rebel icon" aria-hidden="true"></i>
+                  </div>
+                  <h5 class="center">Star Wars Nerd</h5>
+                  <p class="icon-block-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ipsum felis, efficitur ac auctor ut,
+                    ultricies egestas lectus. Fusce eget fringilla urna. Cras pharetra, nisl et venenatis facilisis,
+                  </p>
+                </div>
+              </div>
+              <div class="col s12 m4">
+                <div class="icon-block">
+                  <div class="center">
+                    <i class="fa fa-tree icon" aria-hidden="true"></i>
+                    <!-- <img src="images/mountain-icon.png" width="128" height="128" alt=""> -->
+                  </div>
+                  <h5 class="center">ECA</h5>
+                  <p class="icon-block-text">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ipsum felis, efficitur ac auctor ut,
+                    ultricies egestas lectus. Fusce eget fringilla urna. Cras pharetra, nisl et venenatis facilisis,
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
+            <!-- <p><a href="UK-Cycling-Events/index.php">Click here</a> to check out my latest web project</p> -->
         </div>
       </div>
 
