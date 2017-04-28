a77abeddcad4b42dd1a4395a470a2f2b1a03b8da
Dan Scott <dj-scott1@hotmail.co.uk>
2017-04-28 18:54:12 +0100

    asd

--- includes/sidenav.php
+++ includes/sidenav.php
@@ -20,7 +20,7 @@
           </a>
         </li>
         <li class="sidenav-item">
-          <a href="contact.php">
+          <a class="mail-link" href="mailto:dj-scott1@hotmail.co.uk">
             <img src="images/contact-me-icon-blue.png" class="contact-me-sidenav" width="48" height="48" alt="image not found">
             <span class="sidenav-caption">Contact</span>
           </a>
