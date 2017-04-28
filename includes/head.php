5184dd2ff92d33458e8d6c661e9b75955e9b2ccb
Dan Scott <dj-scott1@hotmail.co.uk>
2017-04-28 18:53:50 +0100

    Changes

--- includes/head.php
+++ includes/head.php
@@ -4,6 +4,7 @@
     <meta http-equiv="cleartype" content="on">
     <meta name="MobileOptimized" content="320">
     <meta name="HandheldFriendly" content="True">
+    <meta name="theme-color" content="#455A64" />
     <meta name="apple-mobile-web-app-capable" content="yes">
     <meta name="description" content="Young and budding web developer - Come check me out!" />
     <meta keywords="Portfolio, Dan Scott, Dan J Scott, Daniel, Daniel Scott, Daniel J Scott, Dan Joseph Scott,
@@ -19,6 +20,7 @@
     <!-- <script src="https://use.fontawesome.com/1a6d4ae9a2.js"></script> -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/slideout/1.0.1/slideout.min.js"></script>
+    <script src="https://use.fontawesome.com/1a6d4ae9a2.js"></script>
 
     <script src="js/main.js"></script>
     <script src="js/materialize.min.js"></script>
@@ -28,3 +30,8 @@
     <link rel="stylesheet" href="css/materialize.min.css">
     <link rel="stylesheet" href="css/slideout.css">
     <link rel="stylesheet" href="css/animsition.min.css">
+
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
